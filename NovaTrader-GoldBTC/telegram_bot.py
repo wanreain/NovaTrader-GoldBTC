2956020b710db33d518afcdd5ecd3b150a3abd4e
wanreain <reduan.alias@outlook.com>
2025-08-01 07:29:10 +0800

    Add files via upload

---
+++ NovaTrader-GoldBTC/telegram_bot.py
@@ -0,0 +1,9 @@
+import os
+import requests
+
+def send_telegram_message(message):
+    token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    data = {"chat_id": chat_id, "text": message}
+    requests.post(url, data=data)
