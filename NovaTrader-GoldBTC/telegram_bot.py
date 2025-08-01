fc82447eb738afb8648e0853a77475c8dac71f71
wanreain <reduan.alias@outlook.com>
2025-08-01 09:14:50 +0800

    Delete NovaTrader-GoldBTC directory

--- NovaTrader-GoldBTC/telegram_bot.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-def send_telegram_message(message):
-    token = os.getenv("TELEGRAM_TOKEN")
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {"chat_id": chat_id, "text": message}
-    requests.post(url, data=data)
